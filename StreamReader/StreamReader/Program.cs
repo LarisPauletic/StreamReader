3e7e8d3f1bfaeca91d55403e0a101be35059ca63
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-04-06 14:38:40 +0200

    Add files via upload

---
+++ StreamReader/StreamReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace CitanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(
+        @"D:\My Documents\NOOP\datoteka.txt");
+            
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
